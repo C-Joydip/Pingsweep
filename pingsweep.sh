77a984d16809120f436c9774c3f555e9a7b9773d
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-09 14:43:10 +0600

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,12 +1,19 @@
 #!bin/bash
 
+echo "Installing Requirements" | lolcat -as 10
+apt update -y
+apt upgrade -y
+apt install ping -y
+apt install ruby -y
+gem install lolcat
+
 echo "To ping an IP type ip
 To scan a Subnet type subnet" 
-echo "What do you wana do? : "
+echo "What do you wana do? : " | lolcat -as 10
 read cnfrm
 if [ "$cnfrm" = "subnet" ];
 then
-	echo "Enter the subnet: "
+	echo "Enter the subnet: " | lolcat -as 10
 	read sb
 	for ipw in $(seq 1 254); do
 	ping -c 1 $sb.$ipw 
@@ -15,7 +22,7 @@ then
 fi
 if [ "$cnfrm" = "ip" ];
 then
-	echo "Enter your ip: "
+	echo "Enter your ip: " | lolcat -as 10
 	read ip
 	ping -c 1 $ip
 	echo "Thanks for using my tool"
