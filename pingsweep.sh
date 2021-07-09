717e42db51e6441932e8a33fb3ca898d153408b8
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-09 23:29:41 +0600

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -11,21 +11,23 @@ echo "To ping an IP type ip
 To scan a Subnet type subnet" 
 echo "What do you wana do? : " | lolcat -as 10
 read cnfrm
-if [ "$cnfrm" = "subnet" ];
-then
-	echo "Enter the subnet: " | lolcat -as 10
-	read sb
-	for ipw in $(seq 1 254); do
-	ping -c 1 $sb.$ipw 
-	done
-	echo "Thanks for using my tool"
-fi
-if [ "$cnfrm" = "ip" ];
-then
-	echo "Enter your ip: " | lolcat -as 10
-	read ip
-	ping -c 1 $ip
-	echo "Thanks for using my tool"
-else
-	echo "Invalid Options"
-fi
+
+case "$cnfrm" in
+	[sS][uU][bB][nN][eE][tT])
+		echo "Enter the subnet: " | lolcat -as 10
+		read sb
+		for ipw in $(seq 1 254); do
+		ping -c 1 $sb.$ipw 
+		done
+		echo "Thanks for using my tool"
+		;;
+	[iI][pP])
+		echo "Enter your ip: " | lolcat -as 10
+		read ip
+		ping -c 1 $ip
+		echo "Thanks for using my tool"
+		;;
+	*)
+		echo "Invalid Options"
+		;;
+esac
